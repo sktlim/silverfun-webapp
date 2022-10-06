cac9a85df418c640c98593978531dc0330e5caa0
htuttttt <sawthidahtut@gmail.com>
2022-10-07 02:55:18 +0800

    changes to activties and healthcare

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,32 +1,14 @@
 import Header from "../components/Header";
-import React from "react"
+import React from "react";
+import Squats from "../assets/icons/Squats.png";
+import Book from "../assets/icons/Book.png";
+import Bus from "../assets/icons/Bus.png";
+import Boxes from  "../components/Boxes";
+import Food from "../assets/icons/Food.png";
 
-// function SliverInfocomm(props){
-//   return(
-//     <iframe width="600" height="400" src="https://data.gov.sg/dataset/silver-infocomm/resource/ce65eab1-37bd-4803-b3ec-2531b2f106cc/view/653be942-0882-4fd2-b2ce-2150d6d70fbe" frameBorder="0"> </iframe>
-//   )
-// }
-
-// function FitnessFacilities(props){
-//   return(
-//     <iframe width="600" height="400" src="https://data.gov.sg/dataset/sportsg-sport-facilities/resource/32a35960-0bf0-4850-a974-638039279510/view/5dc31b56-34e7-4bcb-aa1b-2dfb3eee83e6" frameBorder="0"> </iframe>
-//   )
-// }
-
-// function Libraries(props){
-//   return(
-//     <iframe width="700" height="400" src="https://data.gov.sg/dataset/libraries/resource/5dc4c609-49e7-4bd4-99b7-f43a95675683/view/624a709d-d700-4e55-abba-e3a79a0aa9af" frameBorder="0"> </iframe>
-//   )
-// }
-
-// function Map(props){
-//   const smth = props.smth;
-//   if (smth){
-
-//   }
-// }
 
 function Activities(){
+
   return (
     <div>
     <Header />
@@ -36,9 +18,9 @@ function Activities(){
           <body>Find out more about fun and enriching activities around you!</body>
 
           <br/>
-          <iframe width="700" height="400" src="https://data.gov.sg/dataset/libraries/resource/5dc4c609-49e7-4bd4-99b7-f43a95675683/view/624a709d-d700-4e55-abba-e3a79a0aa9af" frameBorder="0"> </iframe>
+          <iframe width="800" height="400" src="https://data.gov.sg/dataset/libraries/resource/5dc4c609-49e7-4bd4-99b7-f43a95675683/view/624a709d-d700-4e55-abba-e3a79a0aa9af" frameBorder="0"> </iframe>
           <br/>
-
+          
           <div class = "align middle">
               <input
               class = "border-2 border-gray-300 bg-white h-10 px-5 pr-16 w-72 rounded-lg text-sm focus:outline-none shadow"
@@ -46,55 +28,52 @@ function Activities(){
               id="header-search"
               placeholder="Search for activities near you"
               name="s" />
-              
           </div>
           
           <p class = "text-sm text-gray-400 italic">Alternatively, filter through the activities by pressing one of the buttons below.</p>
           
           <br/>
-          <div class = "inline-flex space-x-16">
-            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">Sliver Infocomm Activities</button>
-            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">Fitness Facilities</button>
-            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">Libraries</button>
+          <div class = "inline-flex space-x-9">
+            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
+              <div class = "flex flex-row space-x-1">
+                <img src = {Food} class = "object-scale-down h-25 w-7"/>
+                <div>Hawker Centres</div>
+              </div>
+            </button>
+
+            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
+              <div class = "flex flex-row space-x-1">
+                <img src = {Squats} class = "object-scale-down h-25 w-7"/>
+                <div>Fitness Facilities</div>
+              </div>
+            </button>
+
+            <div>
+            <button class = "bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
+              <div class = "flex flex-row space-x-1">
+                <img src = {Book} class = "object-scale-down h-25 w-7"/>
+                <div>Libraries</div>
+              </div>
+            </button>
+            </div>
           </div>
           
-          <div><br/>
-
-          <button class = "border-2 border-gray-400 bg-white px-5 w-full rounded-lg flex items-center text-left text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
-            Jurong West ActiveSG Sports Centre
-            <br/>
-            Jurong West Street 93
-          </button>
+          <div>
           <br/>
-
-          <button class = "border-2 border-gray-400 bg-white px-5 w-full rounded-lg flex items-center text-left text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
-            Jurong West Public Library
-            <br/>
-            648346 Jurong West Central 3
-          </button>
+          <Boxes icon = {Food} text1 = "Jurong West Hawker Centre" text2 = "Jurong West Street 61" time = "20 Min"></Boxes>
           <br/>
-
-          <button class = "border-2 border-gray-400 bg-white px-5 w-full rounded-lg flex items-center text-left text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
-            Jurong West ActiveSG Sports Centre
-            <br/>
-            Jurong West Street 93
-          </button>
+          <Boxes icon = {Squats} text1 = "Jurong West ActiveSG Sports Centre " text2 = "Jurong West Street 93" time = "23 Min"></Boxes>
           <br/>
-
-          <button class = "border-2 border-gray-400 bg-white px-5 w-full rounded-lg flex items-center text-left text-sm rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
-            Jurong West ActiveSG Sports Centre
-            <br/>
-            Jurong West Street 93
-          </button>
+          <Boxes icon = {Book} text1 = "Jurong West Public Library" text2 = "Jurong West Central 3" time = "25 Min"></Boxes>
+          <br/>
+          <Boxes icon = {Book} text1 = "Jurong West Public Library" text2 = "Jurong West Central 3" time = "25 Min"></Boxes>
           <br/>
 
           </div>
 
-          <body>1 of 5 </body> 
+          <body> 1 of 5 </body> 
           <button class = "text-sm hover:scale-105 "> Next {'>'} </button>
           <br/>
-
-
 
         </p>
       </div>
