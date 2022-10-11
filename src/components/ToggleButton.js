8a964824d81cc0ffb6a7d7cd0013d4c6312adbee
ang-yixuan <angyixuann@gmail.com>
2022-10-11 23:44:54 +0800

    updated notifications page

---
+++ src/components/ToggleButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ToggleButton.css";
+
+const ToggleButton = ({ label }) => {
+return (
+	<div className="container">
+	{label}{" "}
+	<div className="toggle-button">
+		<input type="checkbox" className="checkbox"
+			name={label} id={label} />
+		<label className="label" htmlFor={label}>
+		<span className="inner" />
+		<span className="button" />
+		</label>
+	</div>
+	</div>
+);
+};
+
+export default ToggleButton;
