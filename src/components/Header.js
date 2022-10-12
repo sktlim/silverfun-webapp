cd58900a11a0308499d68ced6e40ec9bc4fcb879
ongjx16 <ongjx16@gmail.com>
2022-10-12 22:39:59 +0800

    minor edits to header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Logo from "../assets/silverfunlogo.svg";
-import ProfileIcon from "../assets/icons/profileIcon.png"
+import ProfileIcon from "../assets/icons/profile.svg"
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div
-      className={`w-full text-black flex items-center justify-between`}
+      className={`w-full text-black flex items-center justify-between mb-3`}
     >
       <Link to="/"><img src={Logo} className="h-13 p-5 px-10" alt="silver fun logo"/></Link>
       <div className={`flex items-center text-lg font-["Rubik"] px-10`}>
@@ -26,7 +26,7 @@ const Header = () => {
           className={`p-8 + ${window.location.pathname === "/Healthcare" || window.location.pathname === "/HealthcareDetails" ? 'underline' : ''}`}>
           HEALTHCARE
         </Link>
-        <Button icon={ProfileIcon} text="PROFILE" classNames={`drop-shadow-none border-2 border-black`} />
+        <Button icon={ProfileIcon} text="PROFILE" classNames={`drop-shadow-none border-2 border-black text-lg`} />
       </div>
     </div>
   );
