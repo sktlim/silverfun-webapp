adc4f65340b01762705c14bc15cab6f70be16b76
ongjx16 <ongjx16@gmail.com>
2022-10-30 23:41:27 +0800

    populated activities page

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -12,6 +12,7 @@ import { library } from "../assets/geoJSON/libraries";
 import { hawker } from "../assets/geoJSON/hawker"
 import { gyms } from "../assets/geoJSON/gyms"
 import axios from 'axios';
+import { displayPartsToString } from "typescript";
 
 
 
@@ -35,11 +36,31 @@ function Activities() {
 
   const [filter, setFilter] = React.useState("library");
   const [isLoading, setLoading] = React.useState(true)
+  const [isRendering, setRendering] = React.useState(true)
 
   const [topLibraries, setTopLibraries] = React.useState(null);
-  const [topHawkers, setTopHawkers] = React.useState(null);
-  const [topFitness, setTopFitness] = React.useState(null);
-  const [display, setDisplay] = React.useState(null);
+
+  const topHawkers = ([
+  ["Boon Lay Place Market and Food Village", "221A Boon Lay Pl, Singapore 641221", "deliveroo.com.sg", ""],
+  ["Jurong West 505 Market & Food Centre", "505 Jurong West Street 52, Singapore 640505", "deliveroo.com.sg", ""],
+  ["Taman Jurong Food Centre", "3 Yung Sheng Rd, Singapore 618499", "foodpanda.sg", ""],
+  ["Bukit Batok Street 11 Food", "155 Bukit Batok Street 11, #01-01, Singapore 650155", "foodpanda.sg", ""],
+  ["Meng Soon Huat Food Centre", "359 Bukit Batok Street 31, #01-401 Block 359, Singapore 650359", "foodpanda.sg", ""], Food, "hawker"])
+
+  const topFitness = ([
+  ["Jurong West ActiveSG Gym", "20 Jurong West Street 93 Singapore 648965", "https://www.myactivesg.com/facilities/jurong-west-activesg-gym", "https://tinyurl.com/yt7w7dkm"],
+  ["Amore Fitness @ Jurong Point 2", "63 Jurong West Central 3, #03-17, Singapore 648886", "https://amorefitness.com.sg", "https://tinyurl.com/34bmdjn5"],
+  ["Jurong East ActiveSG Gym", "21 Jurong East Street 31 Singapore 609517", "https://www.myactivesg.com/facilities/jurong-east-activesg-gym", "https://www.myactivesg.com/-/media/SSC/Consumer/Images/Facilities/Jurong-East-ActiveSG-Gym/Jurong-East-ActiveSG-Gym1.ashx"],
+  ["OMG Yoga", "134 Jurong Gateway Rd, Singapore 600134", "https://www.yogasingapore.net", "https://www.yogasingapore.net/wp-content/uploads/2014/08/privateyoga.jpg"], 
+  ["Gold's Gym @ IBP Jurong East", "2 International Business Park, #01-05, Singapore 609930", "http://www.goldsgym.com.sg/ibp-jurong-east", "https://thegrandstand.com.sg/wp-content/uploads/2019/04/DSC01019.jpg"], Squats, "fitness"])
+
+  const loadingOptions = ([
+  ["loading", "loading", "loading"],
+  ["loading", "loading", "loading"],
+  ["loading", "loading", "loading"],
+  ["loading", "loading", "loading"],
+  ["loading", "loading", "loading"], "loading"])
+  const [display, setDisplay] = React.useState(loadingOptions);
 
   //library
   const libraryData = new Blob([JSON.stringify(library, null, 2)], {
@@ -56,20 +77,20 @@ function Activities() {
     body: libform,
   }
 
-  //hawker
-  const hawkerData = new Blob([JSON.stringify(hawker, null, 2)], {
-    type: "application/json",
-  });
+  // //hawker
+  // const hawkerData = new Blob([JSON.stringify(hawker, null, 2)], {
+  //   type: "application/json",
+  // });
 
-  const hawkerForm = new FormData();
-  hawkerForm.append("data", hawkerData, "libraries.geojson");
-  hawkerForm.append("lat", "1.3483");
-  hawkerForm.append("lng", "103.6831");
+  // const hawkerForm = new FormData();
+  // hawkerForm.append("data", hawkerData, "libraries.geojson");
+  // hawkerForm.append("lat", "1.3483");
+  // hawkerForm.append("lng", "103.6831");
 
-  const hawkeroptions = {
-    method: "POST",
-    body: hawkerForm,
-  }
+  // const hawkeroptions = {
+  //   method: "POST",
+  //   body: hawkerForm,
+  // }
 
   //fitness
   const fitnessData = new Blob([JSON.stringify(gyms, null, 2)], {
@@ -86,20 +107,24 @@ function Activities() {
     body: fitnessForm,
   }
 
+  //response.data[0].properties.Name, response.data[0].properties.ADDRESSBLOCKHOUSENUMBER, response.data[0].properties.ADDRESSSTREETNAME, response.data[0].properties.ADDRESSPOSTALCODE, response.data[0].properties.PHOTOURL
   //library
+
+
   useEffect(() => {
     axios.post("https://silverfun-backend.limsui.repl.co", libform, liboptions)
       .then(response => {
         console.log("libraries");
         // console.log(libraryData)
-        // console.log(response.data);
+        console.log(response.data);
         setTopLibraries(
-          [[response.data[0].properties.Name, response.data[0].properties.ADDRESSSTREETNAME],
-          [response.data[1].properties.Name, response.data[1].properties.ADDRESSSTREETNAME],
-          [response.data[2].properties.Name, response.data[2].properties.ADDRESSSTREETNAME],
-          [response.data[3].properties.Name, response.data[3].properties.ADDRESSSTREETNAME],
-          [response.data[4].properties.Name, response.data[4].properties.ADDRESSSTREETNAME]])
+          [[response.data[0].properties.Name, response.data[0].properties.ADDRESSBLOCKHOUSENUMBER + " "+ response.data[0].properties.ADDRESSSTREETNAME + " Singapore " + response.data[0].properties.ADDRESSPOSTALCODE, response.data[0].properties.HYPERLINK, response.data[0].properties.PHOTOURL],
+          [response.data[1].properties.Name, response.data[1].properties.ADDRESSBLOCKHOUSENUMBER + " "+ response.data[1].properties.ADDRESSSTREETNAME + " Singapore " + response.data[1].properties.ADDRESSPOSTALCODE, response.data[1].properties.HYPERLINK, response.data[1].properties.PHOTOURL],
+          [response.data[2].properties.Name, response.data[2].properties.ADDRESSBLOCKHOUSENUMBER + " "+ response.data[2].properties.ADDRESSSTREETNAME + " Singapore " + response.data[2].properties.ADDRESSPOSTALCODE, response.data[2].properties.HYPERLINK, response.data[2].properties.PHOTOURL],
+          [response.data[3].properties.Name, response.data[3].properties.ADDRESSBLOCKHOUSENUMBER + " "+ response.data[3].properties.ADDRESSSTREETNAME + " Singapore " + response.data[3].properties.ADDRESSPOSTALCODE, response.data[3].properties.HYPERLINK, response.data[3].properties.PHOTOURL],
+          [response.data[4].properties.Name, response.data[4].properties.ADDRESSBLOCKHOUSENUMBER + " "+ response.data[4].properties.ADDRESSSTREETNAME + " Singapore " + response.data[4].properties.ADDRESSPOSTALCODE, response.data[4].properties.HYPERLINK, response.data[4].properties.PHOTOURL], Book, "library"])
         console.log(topLibraries);
+        console.log(display);
         setLoading(false)
       })
       .catch(error => {
@@ -107,20 +132,28 @@ function Activities() {
       });
   }, [])
 
-  //hawker
+  useEffect(() => {
+    console.log(filter)
+    if(filter === "library" && isLoading === false){
+      setDisplay(topLibraries)
+      setRendering(false)
+    }
+    else if(filter === "hawker"){
+      setDisplay(topHawkers)
+      console.log(display)
+    }
+    else if(filter === "fitness"){
+      setDisplay(topFitness)
+      console.log(display)
+    }
+  }, [filter, isLoading]);
+
+  // //hawker
   // useEffect(() => {
   //   axios.post("https://silverfun-backend.limsui.repl.co", hawkerForm, hawkeroptions)
   //     .then(response => {
   //       console.log("hawker");
   //       console.log(response.data);
-  //       setTopHawkers(
-  //         [[response.data[0].properties.Name, response.data[0].properties.ADDRESSSTREETNAME],
-  //         [response.data[1].properties.Name, response.data[1].properties.ADDRESSSTREETNAME],
-  //         [response.data[2].properties.Name, response.data[2].properties.ADDRESSSTREETNAME],
-  //         [response.data[3].properties.Name, response.data[3].properties.ADDRESSSTREETNAME],
-  //         [response.data[4].properties.Name, response.data[4].properties.ADDRESSSTREETNAME]])
-  //       console.log(topHawkers);
-  //       setLoading(false)
   //     })
   //     .catch(error => {
   //       console.log(error);
@@ -134,14 +167,7 @@ function Activities() {
       .then(response => {
         console.log("fitness");
         console.log(response.data);
-        setTopFitness(
-          [[response.data[0].properties.Name, response.data[0].properties.ADDRESSSTREETNAME],
-          [response.data[1].properties.Name, response.data[1].properties.ADDRESSSTREETNAME],
-          [response.data[2].properties.Name, response.data[2].properties.ADDRESSSTREETNAME],
-          [response.data[3].properties.Name, response.data[3].properties.ADDRESSSTREETNAME],
-          [response.data[4].properties.Name, response.data[4].properties.ADDRESSSTREETNAME]])
-        console.log(topFitness);
-        setLoading(false)
+        
       })
       .catch(error => {
         console.log(error);
@@ -149,10 +175,7 @@ function Activities() {
   }, [])
 
 
-  // useEffect(() => {
-  //   // Should not ever set state during rendering, so do this in useEffect instead.
-  //   setTopLibraries(allowedState);
-  // }, []);
+
 
   return (
     <div>
@@ -180,57 +203,57 @@ function Activities() {
 
           <br />
           <div class="inline-flex space-x-9">
-            <button class="bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
+            <button class={`bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md + ${filter === "library" ? 'border-2 border-gray-400 shadow-inner' : 'hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg'}`} onClick={event => setFilter("library")} >
+              <div class="flex flex-row space-x-1">
+                <img src={Book} class="object-scale-down h-25 w-7" />
+                <div>Libraries</div>
+              </div>
+            </button>
+            <button class={`bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md + ${filter === "hawker" ? 'border-2 border-gray-400 shadow-inner' : 'hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg'}`} onClick={event => setFilter("hawker")} >
               <div class="flex flex-row space-x-1">
                 <img src={Food} class="object-scale-down h-25 w-7" />
                 <div>Hawker Centres</div>
               </div>
             </button>
 
-            <button class="bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
+            <button class={`bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md + ${filter === "fitness" ? 'border-2 border-gray-400 shadow-inner' : 'hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg'}`} onClick={event => setFilter("fitness")} >
               <div class="flex flex-row space-x-1">
                 <img src={Squats} class="object-scale-down h-25 w-7" />
                 <div>Fitness Facilities</div>
               </div>
             </button>
 
-            <div>
-              <button class="bg-white h-10 px-5 rounded-lg flex items-center text-center text-md rounded-md  hover:scale-105 transition-all duration-150 ease-linear drop-shadow-lg">
-                <div class="flex flex-row space-x-1">
-                  <img src={Book} class="object-scale-down h-25 w-7" />
-                  <div>Libraries</div>
-                </div>
-              </button>
-            </div>
           </div>
-          {isLoading ? <div>Loading</div> : <div>
+          {isRendering ? <div>Loading</div> : <div>
+      {console.log('display')}
+          {console.log(display)}
             <br />
             <Link to={"/ActivityDetails"}
-              state={{ name: topLibraries[0][0], address: topLibraries[0][1] }}
+              state={{ name: display[0][0], address: display[0][1], link: display[0][2], photo: display[0][3], type: display[6]}}
             >
-              <Boxes icon={Book} text1={topLibraries[0][0]} text2={topLibraries[0][1]} time="20 Min"></Boxes>
+              <Boxes icon={display[5]} text1={display[0][0]} text2={display[0][1]} time="20 Min"></Boxes>
             </Link>
             <br />
             <Link to={"/ActivityDetails"}
-              state={{ name: topLibraries[1][0], address: topLibraries[1][1] }}
+              state={{ name: display[1][0], address: display[1][1], link: display[1][2] , photo: display[1][3], type: display[6]}}
             >
-              <Boxes icon={Book} text1={topLibraries[1][0]} text2={topLibraries[1][1]} time="23 Min"></Boxes></Link>
+              <Boxes icon={display[5]} text1={display[1][0]} text2={display[1][1]} time="23 Min"></Boxes></Link>
             <br />
             <Link to={"/ActivityDetails"}
-              state={{ name: topLibraries[2][0], address: topLibraries[2][1] }}
+              state={{ name: display[2][0], address: display[2][1], link: display[2][2] , photo: display[2][3], type: display[6]}}
             >
-              <Boxes icon={Book} text1={topLibraries[2][0]} text2={topLibraries[2][1]} time="25 Min"></Boxes>
+              <Boxes icon={display[5]} text1={display[2][0]} text2={display[2][1]} time="25 Min"></Boxes>
             </Link>
             <br />
             <Link to={"/ActivityDetails"}
-              state={{ name: topLibraries[3][0], address: topLibraries[3][1] }}
+              state={{ name: display[3][0], address: display[3][1], link: display[3][2] , photo: display[3][3], type: display[6]}}
             >
-              <Boxes icon={Book} text1={topLibraries[3][0]} text2={topLibraries[3][1]} time="25 Min"></Boxes></Link>
+              <Boxes icon={display[5]} text1={display[3][0]} text2={display[3][1]} time="25 Min"></Boxes></Link>
             <br />
             <Link to={"/ActivityDetails"}
-              state={{ name: topLibraries[4][0], address: topLibraries[4][1] }}
+              state={{ name: topLibraries[4][0], address: display[4][1], link: display[4][2] , photo: display[4][3], type: display[6]}}
             >
-              <Boxes icon={Book} text1={topLibraries[4][0]} text2={topLibraries[4][1]} time="25 Min"></Boxes>
+              <Boxes icon={display[5]} text1={display[4][0]} text2={display[4][1]} time="25 Min"></Boxes>
             </Link>
             <br />
 
--- src/pages/ActivityDetails.js
+++ src/pages/ActivityDetails.js
@@ -1,87 +1,91 @@
 import Header from "../components/Header";
 import React from "react"
-import { Link , useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import bicepcurl from "../assets/images/bicepcurl.jpg";
+import cardio from "../assets/images/cardio.jpg";
+import lateralraise from "../assets/images/lateralraise.jpg";
+
 
 function ActivityDetails() {
 
   const location = useLocation();
   console.log(location)
-    return (
-      <div>
+  return (
+    <div>
       <Header />
-      <div className = "flex h-1/3 justify-start">
-        <Link to = "/Activities">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/2889/2889708.png"
-          class="mt-2 mx-8 w-10 h-10"
-          alt=""
-        /></Link>
-        <h1 className = "text-left mb-5">
+      <div className="flex h-1/3 justify-start">
+        <Link to="/Activities">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2889/2889708.png"
+            class="mt-2 mx-8 w-10 h-10"
+            alt=""
+          /></Link>
+        <h1 className="text-left mb-5">
           <text className="text-blue-700">{location.state.name}</text>
         </h1>
       </div>
 
-      <div className = "flex justify-center">
+      <div className="flex justify-center">
 
         {/* left */}
         <div>
           {/* photo */}
           <img
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Jurong_West_Public_Library.jpg"
-          class="py-10 px-10 max-w-2xl h-auto w-full"
-          alt="Jurong West Public Library"
+            src={location.state.photo}
+            class="py-10 px-10 max-w-2xl h-auto w-full"
+            alt="Jurong West Public Library"
           />
           {/* location icon */}
-          <div className = "flex px-10 justify-start">
+          <div className="flex px-10 justify-start">
             <img
-            src="https://cdn-icons-png.flaticon.com/128/450/450016.png"
-            class="w-4 h-4 mt-2"
-            alt="Location icon"
+              src="https://cdn-icons-png.flaticon.com/128/450/450016.png"
+              class="w-4 h-4 mt-2"
+              alt="Location icon"
             />
-            <h3 class = "ml-2">Location</h3>
+            <h3 class="ml-2">Location</h3>
           </div>
 
           {/* location */}
           <div>
-            <p class = "py-5 px-10">
-            60 Jurong West Central 3, #01-03, Singapore 648346
+            <p class="py-5 px-10">
+              {location.state.address}
             </p>
           </div>
 
           {/* JWL location */}
           <div>
             <img
-            src="https://raw.githubusercontent.com/sktlim/silverfun-webapp/hanyu/src/assets/images/JWPL_Map.png"
-            class="px-10 py-5 max-w-2xl h-auto w-full"
-            alt="Jurong West Public Library Google Maps"
+              src="https://raw.githubusercontent.com/sktlim/silverfun-webapp/hanyu/src/assets/images/JWPL_Map.png"
+              class="px-10 py-5 max-w-2xl h-auto w-full"
+              alt="Jurong West Public Library Google Maps"
             />
           </div>
 
         </div>
 
         {/* right */}
-        <div class = "py-10 px-10 w-1/2">
+        <div class="py-10 px-10 w-1/2">
           {/* box */}
-          <div className = "border-2 bg-white px-5 w-full rounded-lg drop-shadow-lg shadow shadow-gray-500/40">
+          <div className="border-2 bg-white px-5 w-full rounded-lg drop-shadow-lg shadow shadow-gray-500/40">
             {/* line 1 */}
-            <div class = "py-2 px-2 text-left underline">
+            <div class="py-2 px-2 text-left underline">
               <h4>Website</h4>
             </div>
             {/* line 2 */}
-            <div class = "flex px-2">
+            <div class="flex px-2">
               {/* globe icon */}
               <img
-              src="https://cdn-icons-png.flaticon.com/512/52/52349.png"
-              class="w-4 h-4 mt-2"
-              alt="Globe icon"
+                src="https://cdn-icons-png.flaticon.com/512/52/52349.png"
+                class="w-4 h-4 mt-2"
+                alt="Globe icon"
               />
               {/* website url */}
-              <p class = "mt-1 px-2">http://www.nlb.gov.sg/main/home</p>
+              <Link class="mt-1 px-2" to = {location.state.link}>{location.state.link}</Link>
               {/* exit icon */}
               <img
-              src="https://cdn-icons-png.flaticon.com/512/3580/3580189.png"
-              class="w-4 h-4 mt-2 ml-28"
-              alt="Exit icon"
+                src="https://cdn-icons-png.flaticon.com/512/3580/3580189.png"
+                class="w-4 h-4 mt-2 ml-28"
+                alt="Exit icon"
               />
             </div>
             {/* line 3 */}
@@ -89,69 +93,110 @@ function ActivityDetails() {
               <h4>Opening Hours</h4>
             </div>
             {/* line 4 */}
-            <div class = "text-left px-2 py-2">
+            <div class="text-left px-2 py-2">
               <p>Monday, 10am-9pm
-              <br/>
-              Tuesday, 10am-9pm
-              <br/>
-              Wednesday, 10am-9pm
-              <br/>
-              Thursday, 10am-9pm
-              <br/>
-              Friday, 10am-9pm
-              <br/>
-              Saturday, 10am-9pm
-              <br/>
-              Sunday, 10am-9pm
-              <br/>
+                <br />
+                Tuesday, 10am-9pm
+                <br />
+                Wednesday, 10am-9pm
+                <br />
+                Thursday, 10am-9pm
+                <br />
+                Friday, 10am-9pm
+                <br />
+                Saturday, 10am-9pm
+                <br />
+                Sunday, 10am-9pm
+                <br />
               </p>
             </div>
           </div>
-
-          <div class = "">
+          {location.state.type == "library" ? <div class="">
             <div>
-              <h3 class = "ml-2 py-5">Recommended Reading Materials</h3>
+              <h3 class="ml-2 py-5">Recommended Reading Materials</h3>
             </div>
 
-            <div class = "flex justify-around space-x-10">
+            <div class="flex justify-around space-x-10">
               {/* book 1 */}
-              <div class = "w-1/3">
+              <div class="w-1/3">
                 <img
-                src="https://img1.od-cdn.com/ImageType-100/0292-1/%7B9FC1BCAA-9AE1-4159-A980-08A51216B61E%7DImg100.jpg"
-                class="py-3 max-w-full h-auto"
-                alt="Book 1"
+                  src="https://img1.od-cdn.com/ImageType-100/0292-1/%7B9FC1BCAA-9AE1-4159-A980-08A51216B61E%7DImg100.jpg"
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 1"
                 />
-                <h6 class = "italic">The Dive</h6>
-                <p class = "text-sm">The Dive is a thrilling narrative nonfiction in the tradition of The Perfect Storm and Apollo 13...</p>
+                <h6 class="italic">The Dive</h6>
+                <p class="text-sm">The Dive is a thrilling narrative nonfiction in the tradition of The Perfect Storm and Apollo 13...</p>
               </div>
               {/* book 2 */}
-              <div class = "w-1/3">
+              <div class="w-1/3">
                 <img
-                src="https://books.google.com.sg/books/publisher/content?id=RkHoCwAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&sig=ACfU3U29MShg2UeMB4iPV9tURNFRAyvfSw&w=1280"
-                class="py-3 max-w-full h-auto"
-                alt="Book 2"
+                  src="https://books.google.com.sg/books/publisher/content?id=RkHoCwAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&sig=ACfU3U29MShg2UeMB4iPV9tURNFRAyvfSw&w=1280"
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 2"
                 />
-                <h6 class = "italic">Up Close with LKY</h6>
-                <p class = "text-sm">Up Close with Lee Kuan Yew gathers some of the vivid memories of 37 people who have worked...</p>
+                <h6 class="italic">Up Close with LKY</h6>
+                <p class="text-sm">Up Close with Lee Kuan Yew gathers some of the vivid memories of 37 people who have worked...</p>
               </div>
               {/* book 3 */}
-              <div class = "w-1/3">
+              <div class="w-1/3">
                 <img
-                src="https://img1.od-cdn.com/ImageType-100/0111-1/%7B7D7C059D-8F3C-4436-A059-B01C602F0823%7DImg100.jpg"
-                class="py-3 max-w-full h-auto"
-                alt="Book 3"
+                  src="https://img1.od-cdn.com/ImageType-100/0111-1/%7B7D7C059D-8F3C-4436-A059-B01C602F0823%7DImg100.jpg"
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 3"
                 />
-                <h6 class = "italic">The Deen Bros</h6>
-                <p class = "text-sm">Imagine getting dinner on the table for your family in forty-five minutes or less...</p>
-                </div>
+                <h6 class="italic">The Deen Bros</h6>
+                <p class="text-sm">Imagine getting dinner on the table for your family in forty-five minutes or less...</p>
+              </div>
             </div>
 
-          </div>
+          </div>: 
+          location.state.type == "fitness" ?
+          <div class="">
+            <div>
+              <h3 class="ml-2 py-5">Recommended Exercises for Seniors</h3>
+            </div>
+
+            <div class="flex justify-around space-x-10">
+              {/* book 1 */}
+              <div class="w-1/3">
+                <img
+                  src={bicepcurl}
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 1"
+                />
+                <h6 class="italic">Bicep Curls</h6>
+                <p class="text-sm">Elbow exercises like bicep curls are important for daily activities such as picking up your grocery bag, lifting up a load of laundry or even your grandchild</p>
+              </div>
+              {/* book 2 */}
+              <div class="w-1/3">
+                <img
+                  src={lateralraise}
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 2"
+                />
+                <h6 class="italic">Stability Ball Exercises</h6>
+                <p class="text-sm">A stability ball can provide a strength-training workout that also emphasizes balance</p>
+              </div>
+              {/* book 3 */}
+              <div class="w-1/3">
+                <img
+                  src={cardio}
+                  class="py-3 max-w-full h-auto"
+                  alt="Book 3"
+                />
+                <h6 class="italic">Cardio</h6>
+                <p class="text-sm">Engaging in aerobic exercise for 60 minutes three times a week can help reduce depressive episodes in the elderly</p>
+              </div>
+            </div>
+
+          </div>: 
+          <div></div>}
+          
         </div>
 
       </div>
 
-      </div>
-    );
+    </div>
+  );
 }
 export default ActivityDetails;
