e14d78778543b7022c389d98350b0bf97447519e
ongjx16 <ongjx16@gmail.com>
2022-11-01 18:01:18 +0800

    removed button from AED

--- src/components/AED.js
+++ src/components/AED.js
@@ -15,7 +15,7 @@ const AED = () => {
               <img src={police} className="px-5" alt="police" />
               <p className="font-['RubikBold'] text-">999</p>
             </div>
-            <button className = "mt-3 mx-3 px-5 py-1 text-xs flex items-center rounded-md hover:scale-105 transition-all duration-150 ease-linear bg-red-300 drop-shadow-lg ">Find AEDs Near Me</button>
+            {/* <button className = "mt-3 mx-3 px-5 py-1 text-xs flex items-center rounded-md hover:scale-105 transition-all duration-150 ease-linear bg-red-300 drop-shadow-lg ">Find AEDs Near Me</button> */}
           </div>
   );
 };
