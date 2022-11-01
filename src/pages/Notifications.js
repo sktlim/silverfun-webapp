17855274219e5dbc174f46c4335ae383f16723d6
Sui Kiat Lim <suikiat00@gmail.com>
2022-11-02 00:44:20 +0800

    changed gmaps back to old

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -12,11 +12,11 @@ import 'react-notifications-component/dist/theme.css'
 function Notifications() {
   const [notificationState, setNotificationState] = React.useState("")
 
-  const notification1 = ()=>{
+  const notification1 = () => {
     addNotification({
       title: 'SilverFun',
       message: 'Notifications enabled',
-      duration:'4000',
+      duration: '4000',
       native: 'true'
     })
 
@@ -33,88 +33,87 @@ function Notifications() {
         onScreen: true
       }
     });
-    
+
     setNotificationState(true)
   }
 
   return (
-      <div>
-        <Header />
-        <ReactNotifications />
-        <div className="flex h-screen">
-          <p className="text-center m-auto">
-            <h1>Alerts and Notifications</h1>
+    <div>
+      <Header />
+      <ReactNotifications />
+      <div className="flex h-screen">
+        <p className="text-center m-auto">
+          <h1>Alerts and Notifications</h1>
+          <br></br>
+          <br></br>
+          <Button classNames={'w-fit bg-white mb-6 drop-shadow-none border border-black opacity-50 text-lg'} text="Notify" onClickHandler={() => notification1()} />
+          <div>
+            <div class="w-full text-black flex items-center justify-between">
+              <h2>Alerts</h2>
+            </div>
             <br></br>
-            <br></br>
-            <Button classNames={'w-fit bg-white mb-6 drop-shadow-none border border-black opacity-50 text-lg'}  text="Notify" onClickHandler={() => notification1()}/>
-            <div>
-              <div class="w-full text-black flex items-center justify-between">
-                <h2>Alerts</h2>
-              </div>
-              <br></br>
 
-              <div class="w-full text-black flex items-center justify-between space-x-3">
-                <h5>Upcoming healthcare appointment</h5>
-                <div>
-                  <React.Fragment>
-                    <ToggleButton label=" " onClick={()=> notification1()}/>
-                  </React.Fragment>
-                </div>
-              </div>
-
-              <br></br>
-              <div class="w-full text-black flex items-center justify-between">
-                <h5>Upcoming courses and events</h5>
-                <div>
-                  <React.Fragment>
-                    <ToggleButton label="  " onClick={()=>notification1()}/>
-                  </React.Fragment>
-                </div>
-              </div>
-
-              <br></br>
-              <br></br>
-              <div class="w-full text-black flex items-center justify-between">
-                <h2>Notifications</h2>
-              </div>
-              <br></br>
-
-              <div class="w-full text-black flex items-center justify-between">
-                <h5>Latest courses and events</h5>
-                <div>
-                  <React.Fragment>
-                    <ToggleButton label="   " onClick={()=> notification1()}/>
-                  </React.Fragment>
-                </div>
-              </div>
-              <br></br>
-
-              <div class="w-full text-black flex items-center justify-between">
-                <h5>New healthcare statistics</h5>
-                <div>
-                  <React.Fragment>
-                    <ToggleButton label="    " onclick={()=>notification1()}/>
-                  </React.Fragment>
-                  <br></br>
-                </div>
+            <div class="w-full text-black flex items-center justify-between space-x-3">
+              <h5>Upcoming healthcare appointment</h5>
+              <div>
+                <React.Fragment>
+                  <ToggleButton label=" " onClick={() => notification1()} />
+                </React.Fragment>
               </div>
             </div>
-          </p>
 
-    </div>
-    <br/>
-    <Link to="/Home">
-      <Stack direction="row" spacing={4}>
-        <Button style={{ marginLeft: 1050 }} variant="outlined" class = "bg-white hover:drop-shadow-lg p-2 rounded-lg">Cancel</Button>
-        <Button variant="contained" class = "bg-yellow rounded-lg text-md p-2 hover:drop-shadow-lg" >Save Changes</Button>
-      </Stack>
-    </Link>
-    <br></br>
-    <br></br>
-    <Footer />
+            <br></br>
+            <div class="w-full text-black flex items-center justify-between">
+              <h5>Upcoming courses and events</h5>
+              <div>
+                <React.Fragment>
+                  <ToggleButton label="  " onClick={() => notification1()} />
+                </React.Fragment>
+              </div>
+            </div>
+
+            <br></br>
+            <br></br>
+            <div class="w-full text-black flex items-center justify-between">
+              <h2>Notifications</h2>
+            </div>
+            <br></br>
+
+            <div class="w-full text-black flex items-center justify-between">
+              <h5>Latest courses and events</h5>
+              <div>
+                <React.Fragment>
+                  <ToggleButton label="   " onClick={() => notification1()} />
+                </React.Fragment>
+              </div>
+            </div>
+            <br></br>
+
+            <div class="w-full text-black flex items-center justify-between">
+              <h5>New healthcare statistics</h5>
+              <div>
+                <React.Fragment>
+                  <ToggleButton label="    " onclick={() => notification1()} />
+                </React.Fragment>
+                <br></br>
+              </div>
+            </div>
+          </div>
+        </p>
+
+      </div>
+      <br />
+      <Link to="/Home">
+        <Stack direction="row" spacing={4}>
+          <Button style={{ marginLeft: 1050 }} variant="outlined" class="bg-white hover:drop-shadow-lg p-2 rounded-lg">Cancel</Button>
+          <Button variant="contained" class="bg-yellow rounded-lg text-md p-2 hover:drop-shadow-lg" >Save Changes</Button>
+        </Stack>
+      </Link>
+      <br></br>
+      <br></br>
+      <Footer />
     </div>
     
-    </router>
     );
 }
 export default Notifications;
